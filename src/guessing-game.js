a40049efdce1b15fe364d45cc988085711562bb3
Nadi Hameza <nadya1gam@gmail.com>
2022-08-16 21:18:13 +0300

    gues

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,34 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.chisla = [];
+        this.chislo;
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
+        this.chisla = [];
 
+
+        for (let i = this.min; i < this.max; i++) {
+            this.chisla.push(i);
+        }
+        return this.chisla;
     }
 
     guess() {
-
+        this.chislo = this.chisla[Math.round(this.chisla.length / 2)];
+        return this.chislo;
     }
 
     lower() {
-
+        this.setRange(this.min, this.chislo);
     }
 
     greater() {
-
+        this.setRange(this.chislo, this.max);
     }
 }
 
